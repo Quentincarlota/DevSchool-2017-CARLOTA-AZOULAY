178b74410b5a51b3a11120badb7cd397783cd075
Jordan Azoulay <jordan.azoulay13@gmail.com>
2016-12-08 18:44:40 +0100

    Go

--- config/app.php
+++ config/app.php
@@ -169,6 +169,7 @@ return [
 
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
+
         /*
          * Application Service Providers...
          */
@@ -224,6 +225,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
 
     ],
 
